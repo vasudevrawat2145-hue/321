b1c1c115e0f9acede8cf35b019fada0e19e9ff90
VASUDEV <vasudevrawat2145@gmail.com>
2026-04-01 13:41:06 +0530

    my first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+print("hello, world!)
+```
+
+**Steo 12:** press **Ctrl + S** to save
+
+**Steo 13:** Create another new file the same way, name it 'README.md'
+
+**Steo 14:** Type something inside it like:
+```
+
+# My First Project
+This is my first project on github.
